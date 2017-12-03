a77802cc242854a20a8e66815cfb0477c54659aa
Leonardo Santos <leon.sdsilva@gmail.com>
2017-12-03 17:34:13 -0200

    feat: communication with api to list the books
    List of books consuming the api, altered project structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import BooksApp from './App'
+import './styles.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <BrowserRouter>
+    <BooksApp />
+  </BrowserRouter>,
+  document.getElementById('root')
+)
